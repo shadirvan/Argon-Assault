5ca627550178d3109aaa49512da09444af09ee73
Shadirvan <shadirvan2021@gmail.com>
2022-08-22 05:30:45 -0700

    Added Clamp to playerShip position

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField]float controlSpeed = 10f;
+    [SerializeField]float xRange = 10f;
+    [SerializeField]float yRange = 7f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +20,14 @@ public class PlayerControls : MonoBehaviour
        float yThrow = Input.GetAxis("Vertical");
 
        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+       float rawXPos = transform.localPosition.x + xOffset;
+       float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+       float rawYPos = transform.localPosition.y + yOffset;
+       float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-       float newXPos = transform.localPosition.x + xOffset;
-       float newYPos = transform.localPosition.y + yOffset;
-
-       transform.localPosition = new Vector3 (newXPos, newYPos,transform.localPosition.z); 
+       transform.localPosition = new Vector3 (clampedXPos, clampedYPos,transform.localPosition.z); 
        
         
     }
